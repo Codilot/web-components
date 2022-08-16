445ffff0cfd9d1ad6e4895fb8a227db9bea5d16e
codilot <30231189+Codilot@users.noreply.github.com>
2022-08-16 15:05:57 +0200

    only parse JSON when a value is set

--- src/helpers/transform.js
+++ src/helpers/transform.js
@@ -1,2 +1,2 @@
-export const parse = (val) => JSON.parse(decodeURIComponent(val));
+export const parse = (val) => (val ? JSON.parse(decodeURIComponent(val)) : "");
 export const stringify = (val) => JSON.stringify(encodeURIComponent(val));
