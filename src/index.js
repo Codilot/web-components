481f008b48a1d8901709cb632607a77d58fce358
codilot <30231189+Codilot@users.noreply.github.com>
2022-08-18 11:27:17 +0200

    improve mapping classes to their components

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import { AppMain } from "./app.js";
 
 customElements.define("app-main", AppMain);
 
+const library = {
+  "base-button": BaseButton,
+  "base-select": BaseSelect,
+};
+
 export const initComponents = (components) => {
   components.forEach((component) => {
     //NOTE: doesn't work as the classname is a string:
@@ -15,15 +20,18 @@ export const initComponents = (components) => {
     //   (m, index, str) => m.replace(/-/, "").toUpperCase()
     // );
     //customElements.define(component, componentClassName);
-    switch (component) {
-      case "base-button":
-        customElements.define(component, BaseButton);
-        break;
-      case "base-select":
-        customElements.define(component, BaseSelect);
-        break;
-      default:
-        break;
-    }
+
+    // NOTE: Former mapping using switch
+    // switch (component) {
+    //   case "base-button":
+    //     customElements.define(component, BaseButton);
+    //     break;
+    //   case "base-select":
+    //     customElements.define(component, BaseSelect);
+    //     break;
+    //   default:
+    //     break;
+    // }
+    customElements.define(component, library[component]);
   });
 };
